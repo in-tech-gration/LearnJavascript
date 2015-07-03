ce90198f4828cff6afd08568c4732363b85475e3
Kostas <kostas@creativecollective.gr>
2015-07-03 09:13:37 +0300

    Create RegExp-Parse-CSS.js

---
+++ snippets/RegExp-Parse-CSS.js
@@ -0,0 +1,13 @@
+var regexpCSS = /([\s\S]*?){([\s\S]*?)}/;
+
+var css   = " .someclass { margin: 10px; padding: 20px; }";
+var match = regexpCSS.exec(css);
+
+console.log( 
+
+    "\n Selector:", match[1], 
+    "\n Declaration Block:", match[2] 
+
+);
+
+/* Reference: http://www.devbattles.com/en/sand/post-1423-Writing+a+CSS+Parser+in+JavaScript */
